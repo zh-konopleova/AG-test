7a791907c88761ae11097dd1e9b7f1c45e316c53
zh-konopleova <konopleova@gmail.com>
2018-11-27 17:32:48 +0300

    Add button to switch layout

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.button').addEventListener('click', function () {
+  document.querySelector('body').classList.toggle('flex');
+  document.querySelector('body').classList.toggle('grid');
+})
